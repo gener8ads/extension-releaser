fe0c1ca4722bac0a658f011036aefa73a663665d
Duncan Allan <duncan@gener8ads.com>
2022-06-06 11:41:41 +0100

    x11 Windowing stuffies

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,55 @@
 
 set -e
 
-mkdir release
+main() {
+    launch_xvfb
+    launch_window_manager
+    signing_operation
+}
 
-unzip -d release release.zip
+launch_xvfb() {
+    # Set defaults if the user did not specify envs.
+    export DISPLAY=${XVFB_DISPLAY:-:1}
+    local screen=${XVFB_SCREEN:-0}
+    local resolution=${XVFB_RESOLUTION:-1280x1024x24}
+    local timeout=${XVFB_TIMEOUT:-5}
 
-jq ". += {\"update_url\": \"$EXTENSION_UPDATE_URL\"}" manifest.json > release/manifest.json
+    # Start and wait for either Xvfb to be fully up or we hit the timeout.
+    Xvfb ${DISPLAY} -screen ${screen} ${resolution} &
+    local loopCount=0
+    until xdpyinfo -display ${DISPLAY} > /dev/null 2>&1
+    do
+        loopCount=$((loopCount+1))
+        sleep 1
+        if [ ${loopCount} -gt ${timeout} ]
+        then
+            echo "${G_LOG_E} xvfb failed to start."
+            exit 1
+        fi
+    done
+}
 
-google-chrome-stable --no-sandbox --pack-extension=./release --pack-extension-key=cert.pem
+launch_window_manager() {
+    local timeout=${XVFB_TIMEOUT:-5}
+
+    # Start and wait for either fluxbox to be fully up or we hit the timeout.
+    fluxbox &
+    local loopCount=0
+    until wmctrl -m > /dev/null 2>&1
+    do
+        loopCount=$((loopCount+1))
+        sleep 1
+        if [ ${loopCount} -gt ${timeout} ]
+        then
+            echo "${G_LOG_E} fluxbox failed to start."
+            exit 1
+        fi
+    done
+}
+
+signing_operation() {
+    mkdir release
+    unzip -d release release.zip
+    jq ". += {\"update_url\": \"$EXTENSION_UPDATE_URL\"}" manifest.json > release/manifest.json
+    google-chrome-stable --no-sandbox --pack-extension=./release --pack-extension-key=cert.pem
+}
