39007e206d1a3e5e5833289c6cdda981cf799825
Duncan Allan <duncan@gener8ads.com>
2022-06-06 10:56:29 +0100

    Unzipping release & installing jq

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+mkdir release
+
+unzip -d release release.zip
+
 jq ". += {\"update_url\": \"$EXTENSION_UPDATE_URL\"}" manifest.json > release/manifest.json
 
 google-chrome-stable --pack-extension=./release --pack-extension-key=cert.pem
